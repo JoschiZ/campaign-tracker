0d4204f668f956e06eff18a2072bb8dd15cb1df6
Joschi <joschi.grey@posteo.de>
2024-10-28 17:30:18 +0100

    UnitTestProject

--- SeasonOfGhosts.Tests/SeasonOfGhosts.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace SeasonOfGhosts.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
--- SeasonOfGhosts/AssemblyInfo.cs
+++ SeasonOfGhosts/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
 using StronglyTypedIds;
 
 [assembly:StronglyTypedIdDefaults(Template.Int, "int-efcore")]
+[assembly:InternalsVisibleTo("SeasonOfGhosts.Tests")]
