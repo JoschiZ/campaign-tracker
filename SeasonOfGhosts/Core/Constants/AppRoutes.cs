35ec41bf2704fdae5a6bcecee88abcedfe2646b9
Joschi <joschi.grey@posteo.de>
2024-10-29 11:32:41 +0100

    General styling work

---
+++ SeasonOfGhosts/Core/Constants/AppRoutes.cs
@@ -0,0 +1,10 @@
+namespace SeasonOfGhosts.Core.Constants;
+
+public static class AppRoutes
+{
+    public const string Home = "/";
+    public const string Characters = "/characters";
+    public const string Stats = "/stats";
+    public const string Factions = "/factions";
+    public const string Settlements = "/settlements";
+}
