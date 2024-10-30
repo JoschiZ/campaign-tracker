72c254d257f407d32af4b9cfee268ee1f22f9d03
Joschi <joschi.grey@posteo.de>
2024-10-30 19:58:39 +0100

    persist last capaign in storage

---
+++ SeasonOfGhosts/Core/Constants/StorageKeys.cs
@@ -0,0 +1,11 @@
+using SeasonOfGhosts.Core.Campaigns;
+
+namespace SeasonOfGhosts.Core.Constants;
+
+public static class StorageKeys
+{
+    /// <summary>
+    /// Contains the <see cref="CampaignId"/> of the last time the user logged in.
+    /// </summary>
+    public const string LastCampaignId = "last_campaign";
+}
