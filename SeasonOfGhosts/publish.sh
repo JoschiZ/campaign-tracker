b1d20c3ceb95b3331d2c5ca1516c23c2166a0f17
Joschi <joschi.grey@posteo.de>
2024-10-31 22:27:42 +0100

    Added publish.sh

---
+++ SeasonOfGhosts/publish.sh
@@ -0,0 +1,10 @@
+dotnet publish
+ssh ubuntu@57.129.70.141
+sudo systemctl stop season-of-ghosts.service
+logout 
+
+scp -r ./bin/Release/net8.0/publish/* ubuntu@57.129.70.141:/var/www/spot_ubuntu/
+
+ssh ubuntu@57.129.70.141
+sudo systemctl start season-of-ghosts.service
+logout 
