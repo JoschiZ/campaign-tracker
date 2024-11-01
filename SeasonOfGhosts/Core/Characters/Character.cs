747a2c7edf5cbbf1146a1b2a2b34b7b32fedd177
Joschi <joschi.grey@posteo.de>
2024-11-01 21:52:29 +0100

    Reworked Characters, added influence

--- SeasonOfGhosts/Core/Characters/Character.cs
+++ SeasonOfGhosts/Core/Characters/Character.cs
@@ -9,26 +9,33 @@ namespace SeasonOfGhosts.Core.Characters;
 
 public sealed class Character
 {
+    public Character(){}
+
+    public Character(int influence, Attitude startingAttitude)
+    {
+        Influence = influence;
+        Attitude = startingAttitude;
+    } 
+
     public CharacterId Id { get; private init; }
     public required string Name { get; set; }
-    public string Ancestry { get; set; } = "";
     
     /// <summary>
     /// A shorter description, mostly a title or profession
     /// </summary>
     public string ShortDescription { get; set; } = "";
-    public string Description { get; set; } = "";
+    public int Influence { get; private set; }
     public Attitude Attitude { get; private set; } = Attitude.Indifferent;
     public required Campaign Campaign { get; init; }
     public List<Faction> Factions { get; init; } = [];
     public List<CharacterLog> Log { get; init; } = [];
     
     
-    public async Task<CharacterLog> ChangeAttitudeAsync(Attitude newAttitude, string reason, SeasonContext context)
+    public async Task<CharacterAttitudeLog> ChangeAttitudeAsync(Attitude newAttitude, string reason, SeasonContext context)
     {
         var character = await context.FindAsync<Character>(Id);
 
-        var log = new CharacterLog()
+        var log = new CharacterAttitudeLog()
         {
             Character = character,
             NewAttitude = newAttitude,
@@ -41,16 +48,24 @@ public sealed class Character
         await context.SaveChangesAsync();
         return log;
     }
-}
 
-public sealed class CharacterLog : ICreationTracking
-{
-    public CharacterLogId Id { get; private init; }
-    public required string Reason { get; init; }
-    public required Attitude NewAttitude { get; init; }
-    public required Character Character { get; init; }
-    public DateTime CreatedAt { get; private init; }
+    public async Task<CharacterInfluenceLog> ChangeInfluence(Adjustment adjustment, SeasonContext context)
+    {
+        var character = await context.FindAsync<Character>(Id);
 
+        var log = new CharacterInfluenceLog()
+        {
+            Character = character,
+            Delta = adjustment.Delta,
+            Reason = adjustment.Reason,
+        };
+        
+        character.Log.Add(log);
+        character.Influence += adjustment.Delta;
+        
+        await context.SaveChangesAsync();
+        return log;
+    }
 }
 
 public sealed class CharacterLogConfiguration : IEntityTypeConfiguration<CharacterLog>
@@ -60,8 +75,30 @@ public sealed class CharacterLogConfiguration : IEntityTypeConfiguration<Charact
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Id).ValueGeneratedOnAdd();
         builder.Property(x => x.Reason).HasMaxLength(100);
+        
+        builder.HasDiscriminator<string>("LogType")
+            .HasValue<CharacterAttitudeLog>("attitude")
+            .HasValue<CharacterInfluenceLog>("influence");
     }
 }
 
 [StronglyTypedId]
 public readonly partial struct CharacterLogId;
+
+public abstract class CharacterLog : ICreationTracking
+{
+    public CharacterLogId Id { get; private init; }
+    public required string Reason { get; init; }
+    public required Character Character { get; init; }
+    public DateTime CreatedAt { get; private init; }
+}
+
+public sealed class CharacterAttitudeLog : CharacterLog
+{
+    public required Attitude NewAttitude { get; init; }
+}
+
+public sealed class CharacterInfluenceLog : CharacterLog
+{
+    public required int Delta { get; init; }
+}
---
+++ SeasonOfGhosts/Migrations/20241101200914_ReworkCharacter.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeasonOfGhosts.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReworkCharacter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ancestry",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Characters");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Influence",
+                table: "Characters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewAttitude",
+                table: "CharacterLog",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Delta",
+                table: "CharacterLog",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogType",
+                table: "CharacterLog",
+                type: "character varying(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Influence",
+                table: "Characters");
+
+            migrationBuilder.DropColumn(
+                name: "Delta",
+                table: "CharacterLog");
+
+            migrationBuilder.DropColumn(
+                name: "LogType",
+                table: "CharacterLog");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Ancestry",
+                table: "Characters",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Characters",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NewAttitude",
+                table: "CharacterLog",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
