4665244b9c103134cdf27a30cbed969c61c589ca
JoschiZ <joschi.grey@posteo.de>
2024-11-25 19:42:09 +0100

    Updated publish.sh

--- SeasonOfGhosts/publish.sh
+++ SeasonOfGhosts/publish.sh
@@ -1,7 +1,7 @@
 dotnet publish
 ssh ubuntu@57.129.70.141 'sudo systemctl stop season-of-ghosts.service'
 
-scp -r ./bin/Release/net8.0/publish/* ubuntu@57.129.70.141:/var/www/spot_ubuntu/
+scp -r ./bin/Release/net9.0/publish/* ubuntu@57.129.70.141:/var/www/spot_ubuntu/
 
 ssh ubuntu@57.129.70.141 'sudo systemctl start season-of-ghosts.service'
 
